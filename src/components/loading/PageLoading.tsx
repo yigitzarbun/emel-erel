f3c12268f31de704439acbdced8904a9819655c0
Yigit Zarbun <yzarbun@gmail.com>
2024-01-03 20:47:42 +0300

    inital commit

--- src/components/loading/PageLoading.tsx
+++ src/components/loading/PageLoading.tsx
@@ -1,5 +1,10 @@
+import styles from "./styles.module.scss";
 const PageLoading = () => {
-  return <div>PageLoading</div>;
+  return (
+    <div className={styles["main-container"]}>
+      <h2>Page Loading</h2>
+    </div>
+  );
 };
 
 export default PageLoading;
