87a7de6674025924367bfde3ea6ba288f558dfe4
Yigit Zarbun <yzarbun@gmail.com>
2024-01-04 09:11:26 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
